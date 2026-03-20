ddae063a36c269f94c2c37ff145c860af5905bfc
Omar Merroun <omar0merroun01@gmail.com>
2026-03-20 04:38:17 +0100

    fix: resolving dependency issue (no vertx.core.Launcher in vertx 5)

--- src/main/java/com/hirzam/MainVerticle.java
+++ src/main/java/com/hirzam/MainVerticle.java
@@ -2,6 +2,7 @@ package com.hirzam;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
 
 public class MainVerticle extends AbstractVerticle {
     
@@ -9,5 +10,10 @@ public class MainVerticle extends AbstractVerticle {
     public void start(Promise<Void> startPromise) {
         System.out.println("Hirzam Chat - Started.");
         startPromise.complete();
+    }
+
+    public static void main(String[] args) {
+    Vertx vertx = Vertx.vertx();
+    vertx.deployVerticle(new MainVerticle());
     }
 }
