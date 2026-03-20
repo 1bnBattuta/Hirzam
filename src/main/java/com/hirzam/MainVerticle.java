5b964d76f97c334d24d1169922996402bcd8295a
Omar Merroun <omar0merroun01@gmail.com>
2026-03-20 03:28:30 +0100

    chore: creating maven project and setting dependencies

---
+++ src/main/java/com/hirzam/MainVerticle.java
@@ -0,0 +1,13 @@
+package com.hirzam;
+
+/**
+ * Hello world!
+ *
+ */
+public class MainVerticle 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
