4c4a573a426481c46414730df9529c84b9379c55
Omar Merroun <omar0merroun01@gmail.com>
2026-03-28 22:52:16 +0100

    feat: adding bonus database methods to message service interface

--- src/main/java/com/hirzam/service/MessageService.java
+++ src/main/java/com/hirzam/service/MessageService.java
@@ -5,6 +5,11 @@ import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 
 public interface MessageService {
-    Future<JsonArray> getLastMessages();
-    Future<JsonObject> addMessage(String uname, String content);
+
+    Future<JsonArray>  getLastMessages();
+    Future<JsonObject> addMessage(String username, String content);
+
+    Future<JsonObject> getMessage(int id);
+    Future<JsonObject> updateMessage(int id, String content);
+    Future<Void>       deleteMessage(int id);
 }
